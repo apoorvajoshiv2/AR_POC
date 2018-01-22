04aac12a497b3df2c64902c133dff145b4ed20ce
apoorva.joshi <apoorva.joshi@v2solutions.com>
2018-01-22 14:53:28 +0530

    Refactoring code

--- ARPlanetsRotation/ViewController.swift
+++ ARPlanetsRotation/ViewController.swift
@@ -22,140 +22,90 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.sceneView.session.run(configuration)
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(objectTapped))
         self.sceneView.addGestureRecognizer(tapGesture)
+        self.sceneView.automaticallyUpdatesLighting = true
         showPlanets()
     }
 
-    func setPlanet(geometry: SCNGeometry, diffuse: UIImage?, specular: UIImage?, emission: UIImage?, normal: UIImage?, position: SCNVector3) -> SCNNode {
-        let node = SCNNode(geometry: geometry)
-        node.geometry?.firstMaterial?.diffuse.contents = diffuse
-        node.geometry?.firstMaterial?.emission.contents = emission
-        node.position = position
-        return node
-    }
-    
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func showPlanets() {
-        let sun = setPlanet(geometry: SCNSphere(radius: 0.5), diffuse: #imageLiteral(resourceName: "Sun diffuse"), specular: nil, emission: nil, normal: nil, position: parentPosition)
-        //        sun.position = SCNVector3(0, 0, -4)
+        let sun = SCNNode(geometry: SCNSphere(radius: 0.5))
+        sun.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Sun diffuse")
+        sun.position = parentPosition
         self.sceneView.scene.rootNode.addChildNode(sun)
         sun.name = "sun"
         
         // Added a parent node for earth to rotate earth at it's own pace rather than at sun's rotation speed
-        let earthParentNode = SCNNode()
-        earthParentNode.position = parentPosition
-        let earth = setPlanet(geometry: SCNSphere(radius: 0.2), diffuse: #imageLiteral(resourceName: "Earth diffuse"), specular: #imageLiteral(resourceName: "Earth specular"), emission: #imageLiteral(resourceName: "Earth emission"), normal: #imageLiteral(resourceName: "Earth normal"), position: SCNVector3(1.8, 0, 0))
-        //        earth.position = SCNVector3(2, 0, 0)
-        earthParentNode.addChildNode(earth)
-        self.sceneView.scene.rootNode.addChildNode(earthParentNode)
-        let earthRotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 20)
-        earthParentNode.runAction(SCNAction.repeatForever(earthRotation))
+        
+        let earth = placeThePlanet(geometry: SCNSphere(radius: 0.2), diffuse: #imageLiteral(resourceName: "Earth diffuse"), specular: #imageLiteral(resourceName: "Earth specular"), emission: #imageLiteral(resourceName: "Earth emission"), normal: #imageLiteral(resourceName: "Earth normal"), position: SCNVector3(1.8, 0, 0), planetRotationDuration: 80, planetRevolutionDuration: 25, planetName: "earth")
         
         // Rotation of the moon around the earth
         let moonParentNode = SCNNode()
         moonParentNode.position = SCNVector3(1.8, 0, 0)
-        let moon = setPlanet(geometry: SCNSphere(radius: 0.04), diffuse: #imageLiteral(resourceName: "Moon diffuse") , specular: nil, emission: nil, normal: nil, position: SCNVector3(0.3, 0, 0))
-        //        moon.position = SCNVector3(0.4, 0, 0)
+        let moon = SCNNode(geometry: SCNSphere(radius: 0.04))
+        moon.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Moon diffuse")
+        moon.position = SCNVector3(0.4, 0, 0)
         moonParentNode.addChildNode(moon)
-        earthParentNode.addChildNode(moonParentNode)
+        earth.parent?.addChildNode(moonParentNode)
         let moonRotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 15)
         moonParentNode.runAction(SCNAction.repeatForever(moonRotation))
         moon.name = "moon"
         
-        // Revolution of the earth around itself
-        let earthRevolution = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 25)
-        earth.runAction(SCNAction.repeatForever(earthRevolution))
-        earth.name = "earth"
-        
         // Added a parent node for venus to rotate it at it's own pace rather than at sun's rotation speed
-        let venusParentNode = SCNNode()
-        venusParentNode.position = parentPosition
-        let venus = setPlanet(geometry: SCNSphere(radius: 0.14), diffuse: #imageLiteral(resourceName: "Venus diffuse"), specular: nil, emission: #imageLiteral(resourceName: " Venus emission"), normal: nil, position: SCNVector3(1.1 ,0, 0))
-        venusParentNode.addChildNode(venus)
-        self.sceneView.scene.rootNode.addChildNode(venusParentNode)
-        let venusRevolution = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 25)
-        venus.runAction(SCNAction.repeatForever(venusRevolution))
-        let venusRotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 15)
-        venusParentNode.runAction(SCNAction.repeatForever(venusRotation))
-        venus.name = "venus"
+        
+        _ = placeThePlanet(geometry: SCNSphere(radius: 0.14), diffuse: #imageLiteral(resourceName: "Venus diffuse"), specular: nil, emission: nil, normal: nil, position: SCNVector3(1.1 ,0, 0), planetRotationDuration: 50, planetRevolutionDuration: 25, planetName: "venus")
         
         // Mars
-        let marsParentNode = SCNNode()
-        marsParentNode.position = parentPosition
-        let mars = setPlanet(geometry: SCNSphere(radius: 0.25), diffuse: #imageLiteral(resourceName: "Mars diffuse"), specular: nil, emission: #imageLiteral(resourceName: " Venus emission"), normal: nil, position: SCNVector3(2.5 ,0, 0))
-        marsParentNode.addChildNode(mars)
-        self.sceneView.scene.rootNode.addChildNode(marsParentNode)
-        let marsRevolution = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 25)
-        mars.runAction(SCNAction.repeatForever(marsRevolution))
-        let marsRotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 30)
-        marsParentNode.runAction(SCNAction.repeatForever(marsRotation))
-        mars.name = "mars"
+        
+        _ = placeThePlanet(geometry: SCNSphere(radius: 0.25), diffuse: #imageLiteral(resourceName: "Mars diffuse"), specular: nil, emission: nil, normal: nil, position: SCNVector3(2.5 ,0, 0), planetRotationDuration: 95, planetRevolutionDuration: 25, planetName: "mars")
         
         // Jupitor
-        let jupitorParentNode = SCNNode()
-        jupitorParentNode.position = parentPosition
-        let jupitor = setPlanet(geometry: SCNSphere(radius: 0.5), diffuse: #imageLiteral(resourceName: "Jupitor diffuse"), specular: nil, emission: #imageLiteral(resourceName: " Venus emission"), normal: nil, position: SCNVector3(3.7 ,0, 0))
-        jupitorParentNode.addChildNode(jupitor)
-        self.sceneView.scene.rootNode.addChildNode(jupitorParentNode)
-        let jupitorRevolution = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 25)
-        jupitor.runAction(SCNAction.repeatForever(jupitorRevolution))
-        let jupitorRotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 20)
-        jupitorParentNode.runAction(SCNAction.repeatForever(jupitorRotation))
-        jupitor.name = "jupitor"
+        
+        _ = placeThePlanet(geometry: SCNSphere(radius: 0.5), diffuse: #imageLiteral(resourceName: "Jupitor diffuse"), specular: nil, emission: nil, normal: nil, position: SCNVector3(3.7 ,0, 0), planetRotationDuration: 120, planetRevolutionDuration: 25, planetName: "jupitor")
         
         // Mercury
-        let mercuryParentNode = SCNNode()
-        mercuryParentNode.position = parentPosition
-        let mercury = setPlanet(geometry: SCNSphere(radius: 0.1), diffuse: #imageLiteral(resourceName: "Mercury diffuse"), specular: nil, emission: #imageLiteral(resourceName: " Venus emission"), normal: nil, position: SCNVector3(0.7 ,0, 0))
-        mercuryParentNode.addChildNode(mercury)
-        self.sceneView.scene.rootNode.addChildNode(mercuryParentNode)
-        let mercuryRevolution = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 9)
-        mercury.runAction(SCNAction.repeatForever(mercuryRevolution))
-        let mercuryRotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 9)
-        mercuryParentNode.runAction(SCNAction.repeatForever(mercuryRotation))
-        mercury.name = "mercury"
+        
+        _ = placeThePlanet(geometry: SCNSphere(radius: 0.1), diffuse: #imageLiteral(resourceName: "Mercury diffuse"), specular: nil, emission: nil, normal: nil, position: SCNVector3(0.7 ,0, 0), planetRotationDuration: 40, planetRevolutionDuration: 9, planetName: "mercury")
         
         // Saturn
-        let saturnParentNode = SCNNode()
-        saturnParentNode.position = parentPosition
-        let saturn = setPlanet(geometry: SCNSphere(radius: 0.45), diffuse: #imageLiteral(resourceName: "Saturn diffuse"), specular: nil, emission: #imageLiteral(resourceName: " Venus emission"), normal: nil, position: SCNVector3(5.5 ,0, 0))
-        let saturnRing = setPlanet(geometry: SCNTorus(ringRadius: 0.6, pipeRadius: 0.02), diffuse: #imageLiteral(resourceName: "Saturn Ring"), specular: nil, emission: nil, normal: nil, position: SCNVector3(0, 0, 0))
+        
+        let saturn = placeThePlanet(geometry: SCNSphere(radius: 0.45), diffuse: #imageLiteral(resourceName: "Saturn diffuse"), specular: nil, emission: nil, normal: nil, position: SCNVector3(5.5 ,0, 0), planetRotationDuration: 100, planetRevolutionDuration: 25, planetName: "saturn")
+        let saturnRing = SCNNode(geometry: SCNTorus(ringRadius: 0.6, pipeRadius: 0.02))
+        saturnRing.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Saturn Ring")
+        saturnRing.position = SCNVector3(0, 0, 0)
         saturn.addChildNode(saturnRing)
-        saturnParentNode.addChildNode(saturn)
-        self.sceneView.scene.rootNode.addChildNode(saturnParentNode)
-        let saturnRevolution = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 25)
-        saturn.runAction(SCNAction.repeatForever(saturnRevolution))
-        let saturnRotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 28)
-        saturnParentNode.runAction(SCNAction.repeatForever(saturnRotation))
-        saturn.name = "saturn"
         
         // Uranus
-        let uranusParentNode = SCNNode()
-        uranusParentNode.position = parentPosition
-        let uranus = setPlanet(geometry: SCNSphere(radius: 0.3), diffuse: #imageLiteral(resourceName: "Uranus diffuse"), specular: nil, emission: #imageLiteral(resourceName: " Venus emission"), normal: nil, position: SCNVector3(6.5 ,0, 0.5))
-        uranusParentNode.addChildNode(uranus)
-        self.sceneView.scene.rootNode.addChildNode(uranusParentNode)
-        let uranusRevolution = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 25)
-        uranus.runAction(SCNAction.repeatForever(uranusRevolution))
-        let uranusRotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 15)
-        uranusParentNode.runAction(SCNAction.repeatForever(uranusRotation))
-        uranus.name = "uranus"
-       
+        
+        _ = placeThePlanet(geometry: SCNSphere(radius: 0.3), diffuse: #imageLiteral(resourceName: "Uranus diffuse"), specular: nil, emission: nil, normal: nil, position: SCNVector3(6.5 ,0, 0.5), planetRotationDuration: 115, planetRevolutionDuration: 25, planetName: "uranus")
+        
         // Neptune
-        let neptuneParentNode = SCNNode()
-        neptuneParentNode.position = parentPosition
-        let neptune = setPlanet(geometry: SCNSphere(radius: 0.3), diffuse: #imageLiteral(resourceName: "Neptune diffuse"), specular: nil, emission: #imageLiteral(resourceName: " Venus emission"), normal: nil, position: SCNVector3(7.5 ,0, 1))
-        neptuneParentNode.addChildNode(neptune)
-        self.sceneView.scene.rootNode.addChildNode(neptuneParentNode)
-        let neptuneRevolution = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 25)
-        neptune.runAction(SCNAction.repeatForever(neptuneRevolution))
-        let neptuneRotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 24)
-        neptuneParentNode.runAction(SCNAction.repeatForever(neptuneRotation))
-        neptune.name = "neptune"
+        
+        _ = placeThePlanet(geometry: SCNSphere(radius: 0.3), diffuse: #imageLiteral(resourceName: "Neptune diffuse"), specular: nil, emission: nil, normal: nil, position: SCNVector3(7.5 ,0, 1), planetRotationDuration: 135, planetRevolutionDuration: 25, planetName: "neptune")
+
     }
+    
+    func placeThePlanet(geometry: SCNGeometry, diffuse: UIImage?, specular: UIImage?, emission: UIImage?, normal: UIImage?, position: SCNVector3, planetRotationDuration: TimeInterval?, planetRevolutionDuration: TimeInterval?, planetName: String?) -> SCNNode {
+        let parentNode = SCNNode()
+        parentNode.position = parentPosition
+        let node = SCNNode(geometry: geometry)
+        node.geometry?.firstMaterial?.diffuse.contents = diffuse
+        node.geometry?.firstMaterial?.emission.contents = emission
+        node.position = position
+        parentNode.addChildNode(node)
+        self.sceneView.scene.rootNode.addChildNode(parentNode)
+        let planetRevolution = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: planetRevolutionDuration!)
+        node.runAction(SCNAction.repeatForever(planetRevolution))
+        let planetRotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: planetRotationDuration!)
+        parentNode.runAction(SCNAction.repeatForever(planetRotation))
+        node.name = planetName
+        return node
+    }
+    
     
     @objc func objectTapped(sender: UITapGestureRecognizer) {
         let boxTapped = sender.view as! SCNView
@@ -165,7 +115,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             print("No object found")
         } else {
             let object = hitTest.first
-            print("name of the object ",object?.node.name)
             planetName = (object?.node.name)!
             performSegue(withIdentifier: "segueToShow", sender: self)
         }
